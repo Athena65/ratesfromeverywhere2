2d11c2212edbbae63d72836c06b7bd2e48f92963
Athena65 <burkis_@hotmail.com>
2024-10-28 23:58:59 +0300

    ana icerik products blade php ye eklendi

---
+++ resources/views/products.blade.php
@@ -0,0 +1,25 @@
+<!-- Ana İçerik -->
+<main class="container my-5">
+    <h2 class="text-center mb-4">Ürünlerimiz</h2>
+    <div class="row">
+        @forelse($products as $product)
+            <div class="col-md-4 mb-4">
+                <div class="card h-100">
+                    @if($product->image)
+                        <img src="{{ asset('storage/' . $product->image) }}" class="card-img-top" alt="{{ $product->name }}">
+                    @else
+                        <img src="https://via.placeholder.com/150" class="card-img-top" alt="Resim yok">
+                    @endif
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $product->name }}</h5>
+                        <p class="card-text">{{ $product->description }}</p>
+                        <p><strong>Site İçi Değerlendirme:</strong> {{ $product->site_rating }} / 5</p>
+                        <p><strong>Genel Değerlendirme:</strong> {{ $product->global_rating }} / 5</p>
+                    </div>
+                </div>
+            </div>
+        @empty
+            <p class="text-center">Henüz ürün yok. Yakında burada ürünlerimizi görebileceksiniz!</p>
+        @endforelse
+    </div>
+</main>
